34e4e1e087421b98be6a442f1c441d9b6f9b79c6
Marco Carcini <marco.carcini@hotmail.com>
2020-10-23 23:59:30 -0500

    Adding ScrollSpy

--- js/scrolling.js
+++ js/scrolling.js
@@ -2,4 +2,17 @@ $(function(){
     $(window).on("load resize", function(){
         $(".fill-screen").css("height", window.innerHeight);
     });
+
+    $('body').scrollspy({ 
+        target: '.navbar' ,
+        offset:160
+    })
+
+    //smooth scrolling
+    $('nav a').bind('click', function(){
+        $('html, body').stop().animate({
+            scrollTop:$($(this).attr('href')).offset().top-110
+        }, 1500, 'easeInOutExpo');
+
+    });
 });
